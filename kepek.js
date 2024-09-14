e5a1bf019c84965a4072a84b99c224930ab770c7
minaii93 <hajnalkaminaei@gmail.com>
2024-09-14 11:18:27 +0200

    kepek

--- kepek.js
+++ kepek.js
@@ -3,33 +3,29 @@ kepek = [
     {
         cim : "kep1", 
         leiras: "Lorem ipsum", 
-        eleresiUt: ""
+        eleresiUt: href="/kepek/filipe-freitas-TV0JFWli5gs-unsplash.jpg"
     },
     {
         cim : "kep1", 
         leiras: "Lorem ipsum", 
-        eleresiUt: ""
+        eleresiUt: href="/kepek/jimmy-woo-5MbZgUmzGg0-unsplash.jpg"
     },
     {
         cim : "kep1", 
         leiras: "Lorem ipsum", 
-        eleresiUt: ""
+        eleresiUt: href="/kepek/taryn-kaahanui-ECzqBXclKhQ-unsplash.jpg"
     },
     {
         cim : "kep1", 
         leiras: "Lorem ipsum", 
-        eleresiUt: ""
+        eleresiUt: href="/kepek/wolfgang-hasselmann-ULfobILrTpM-unsplash.jpg"
     },
     {
         cim : "kep1", 
         leiras: "Lorem ipsum", 
-        eleresiUt: ""
+        eleresiUt: href="/kepek/zosia-szopka-j5HQf4MpXZQ-unsplash.jpg"
     },
-    {
-        cim : "kep1", 
-        leiras: "Lorem ipsum", 
-        eleresiUt: ""
-    },
+ 
 
 
 ];
