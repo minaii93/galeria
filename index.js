e85d55c8fb51d75b063493776e2fb28f68d3f66f
minaii93 <hajnalkaminaei@gmail.com>
2024-09-17 15:58:28 +0200

    html modositas

---
+++ index.js
@@ -0,0 +1,8 @@
+import {kepek} from "./kepek.js";
+
+import jatekter from "./jatekter.js";
+
+const kivalasztottKep = [];
+
+const divElem = $(".kepTarolo");
+const kivElem = $(".kisKepek");
--- jatekter.js
+++ jatekter.js
@@ -1,3 +1,7 @@
+//Készíts egy Játéktér osztályt, mely a kisképek megjelenítésére szolgál. 4 kép legyen egymás mellett alapértelmezetten, majd 3, majd 2, mobilnézetben a galériaképek egymás alatt helyezkedjenek el. 
+
+//A játéktér felett legyen az éppen aktuális nagy kép. Ehhez használd a már meglévő Kartya osztályt. 
+
 export default class Jatekter{
 
 }
