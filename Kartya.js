32c887887eac8cf666082b9b3885773ef085b5f3
minaii93 <hajnalkaminaei@gmail.com>
2024-09-19 11:38:50 +0200

    js

--- Kartya.js
+++ Kartya.js
@@ -1,4 +1,23 @@
 export default class Kartya {
     //Készíts egy Kartya osztályt, amely egyetlen kép megjelenítésére szolgál. 
+#kep = {}
 
+constructor(kep, szuloElem, esemenyKezelo) {
+    this.#kep = kep;
+    this.szuloElem = szuloElem;
+    this.#kepLetrehoz();
+    this.#esemenyKezelo();
+    
+    
+};
+
+#esemenyKezelo(){
+
+};
+
+#kepLetrehoz(){
+    this.szuloElem.append(`
+        
+        `)
+};
 }
